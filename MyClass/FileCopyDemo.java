9b4e8e44c02fddad6cd26596f6ab62b03ef26a71
WheatleyZ <zhengsl7749@live.com>
2016-09-30 08:17:01 +0800

    usingBufferedReader/Writer

--- MyClass/FileCopyDemo.java
+++ MyClass/FileCopyDemo.java
@@ -7,7 +7,7 @@ public class FileCopyDemo {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 		
-//		运用字节流复制文件Source.txt到byte copiedfile.txt
+//		用字节流复制文件Source.txt到byte copiedfile.txt
 		FileInputStream fis = new FileInputStream(path+fileName);
 		FileOutputStream fos = new FileOutputStream(path+"byte "+destFileName);
 		byte[] b = new byte[1024*1024];
@@ -18,18 +18,22 @@ public class FileCopyDemo {
 		fis.close();
 		fos.close();
 		
-//		运用字符流复制文件Source.txt到writer copiedfile.txt
+//		用字符流复制文件Source.txt到byte copiedfile.txt
 		FileReader fr = new FileReader(path+fileName);
 		FileWriter fw = new FileWriter(path+"writer "+destFileName);
+		BufferedReader br = new BufferedReader(fr);
+		BufferedWriter bw = new BufferedWriter(fw);
 		int len = 0;
 		char[] c = new char[1024*1024];
-		while ((len=fr.read(c)) != -1){
+		while ((len=br.read(c)) != -1){
 			for(int i=0;i<len;i++){
 				System.out.println(c[i]);
 			}
-			fw.write(c,0,len);
-			fw.flush();
+			bw.write(c,0,len);
+			bw.flush();
 		}
+		br.close();
+		bw.close();
 		fr.close();
 		fw.close();
 	}
