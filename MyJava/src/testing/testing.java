5956647f4c0c4d9cf08e119b9a48886a4c887b70
WheatleyZ <zhengsl7749@live.com>
2017-03-24 14:51:03 +0800

    update

--- MyJava/src/testing/testing.java
+++ MyJava/src/testing/testing.java
@@ -2,13 +2,28 @@ package testing;
 
 public class testing {
 	public static void main(String[] args) {
-		long a = -619991808;
-		while (a < 0)
-			a += 0x100000000l;
-		int[] temp = { (int) ((a / 0x1000000) % 0x100), (int) (((a % 0x1000000 - a % 0x1000) / 0x10000) % 0x100),
-				(int) (((a % 0x10000 - a % 0x100) / 0x100) % 0x100), (int) (a % 0x100) };
-		for (int i : temp) {
-			System.out.println(Integer.toHexString(i));
+		int[][] state = { { 11, 12, 13, 14 }, { 21, 22, 23, 24 }, { 31, 32, 33, 34 }, { 41, 42, 43, 44 } };
+		shiftRows(state);
+		for (int[] x : state) {
+			for (int y : x) {
+				System.out.println(y);
+			}
 		}
+	}
+
+	private static void shiftRows(int[][] state) {
+		for(int i=0;i<4;i++){
+			for(int j=i;j>0;j--){
+				shiftOnce(state,i);
+			}
+		}
+	}
+
+	static void shiftOnce(int[][] state, int row) {
+		int temp = state[0][row];
+		for (int i = 0; i < 3; i++) {
+			state[i][row] = state[i + 1][row];
+		}
+		state[3][row] = temp;
 	}
 }
