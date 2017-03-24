0655191c2864fd5347092130f596f037f396dbee
WheatleyZ <zhengsl7749@live.com>
2017-03-24 14:46:07 +0800

    DES Homework

---
+++ MyJava/src/desTest/DESTest.java
@@ -0,0 +1,20 @@
+package desTest;
+
+public class DESTest {
+	static int[] plainText = new int[64];
+	static int[][] key = new int[2][28];
+
+	public static void main(String[] args) {
+
+	}
+	
+	static void enCipher(){
+		
+	}
+	
+	static void deCipher(){
+		
+	}
+	
+	
+}
