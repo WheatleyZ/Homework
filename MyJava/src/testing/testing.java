7943a2fd84ca62f1995449ef18a1e36a25bd3b5a
Zheng <Zheng@windows10.microdone.cn>
2017-03-24 14:56:29 +0800

    update

--- MyJava/src/testing/testing.java
+++ MyJava/src/testing/testing.java
@@ -2,13 +2,24 @@ package testing;
 
 public class testing {
 	public static void main(String[] args) {
-		long a = -619991808;
-		while (a < 0)
-			a += 0x100000000l;
-		int[] temp = { (int) ((a / 0x1000000) % 0x100), (int) (((a % 0x1000000 - a % 0x1000) / 0x10000) % 0x100),
-				(int) (((a % 0x10000 - a % 0x100) / 0x100) % 0x100), (int) (a % 0x100) };
-		for (int i : temp) {
-			System.out.println(Integer.toHexString(i));
+		System.out.println(Integer.toHexString(fieldMulti(0x57, 0x13)));
+	}
+
+	private static int fieldMulti(int a, int b) {
+		int answer = 0;
+		int[] aMulti = new int[8];
+		aMulti[0] = a;
+		for (int i = 1; i < 8; i++) {
+			aMulti[i] = aMulti[i - 1] * 2 % 0x100;
+			if (aMulti[i - 1] > 0x7f)
+				aMulti[i] ^= 0x1b;
 		}
+		String bBinary = Integer.toBinaryString(b);
+		for (int i = bBinary.length() - 1; i >= 0; i--) {
+			if (bBinary.charAt(i) == '1') {
+				answer ^= aMulti[bBinary.length() - i - 1];
+			}
+		}
+		return answer;
 	}
 }
