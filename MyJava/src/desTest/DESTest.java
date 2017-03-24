f427de068cfdfcf8489d28b24fd3f03fa2297d55
Zheng <Zheng@windows10.microdone.cn>
2017-03-25 00:51:08 +0800

    update:key generate

--- MyJava/src/desTest/DESTest.java
+++ MyJava/src/desTest/DESTest.java
@@ -1,20 +1,66 @@
 package desTest;
 
+import java.math.BigInteger;
+import java.lang.Exception;
+
 public class DESTest {
+	static int[] pc1 = { 57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18, 10, 2, 59, 51, 43, 35, 27, 19, 11, 3, 60,
+			52, 44, 36, 63, 55, 47, 39, 31, 23, 15, 7, 62, 54, 46, 38, 30, 22, 14, 6, 61, 53, 45, 37, 29, 21, 13, 5, 28,
+			20, 12, 4 };
+	static int[] pc2 = { 14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10, 23, 19, 12, 4, 26, 8, 16, 7, 27, 20, 13, 2, 41, 52,
+			31, 37, 47, 55, 30, 40, 51, 45, 33, 48, 44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32 };
 	static int[] plainText = new int[64];
-	static int[][] key = new int[2][28];
+	static String key;
+	static int[] keySrc;
+	static int[] finalKey = new int[16];
 
 	public static void main(String[] args) {
+		try {
+			keyGenenate(key);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void keyGenenate(String key) throws Exception {
+		if (key.length() != 64)
+			throw new Exception();
+		keySrc = new int[56];
+		key = Integer.toBinaryString(Integer.parseInt(key));
+		for (int i = 0; i < 56; i++) {
+			keySrc[i] = Integer.parseInt(Character.toString(key.charAt(pc1[i])));
+		}
+		generate(keySrc);
+	}
+
+	private static void generate(int[] keySrc) {
+		for (int i = 0; i < 16; i++) {
+			leftShift(keySrc);
+			int temp = keySrc[pc2[0]];
+			for (int j = 1; j < 48; j++) {
+				temp *= 2;
+				temp += keySrc[pc2[j]];
+			}
+			finalKey[i] = temp;
+		}
+	}
+
+	private static void leftShift(int[] keySrc) {
+		for (int i = 0; i < 2; i++) {
+			int temp = keySrc[28 * i];
+			for (int j = 0; j < 27; j++) {
+				keySrc[28 * i + j] = keySrc[28 * i + j + 1];
+			}
+			keySrc[28 * i + 27] = temp;
+		}
+	}
+
+	static void enCipher() {
 
 	}
-	
-	static void enCipher(){
-		
+
+	static void deCipher() {
+
 	}
-	
-	static void deCipher(){
-		
-	}
-	
-	
+
 }
