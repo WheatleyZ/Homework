897c18389d168d48aa0853089e50f8505d6cbc0b
WheatleyZ <zhengsl7749@live.com>
2017-04-08 18:09:07 +0800

    update new homework

---
+++ MyJava/src/hmac/HMACTest.java
@@ -0,0 +1,10 @@
+package hmac;
+
+public class HMACTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ MyJava/src/wiener低解密指数攻击/WienerTest.java
@@ -0,0 +1,10 @@
+package wiener低解密指数攻击;
+
+public class WienerTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
