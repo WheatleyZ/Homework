d43c3cb7cda06a7a64117c91979f2d49a80abe1f
ZhengShaolong <zhengsl7749@live.com>
2017-04-24 13:43:44 +0800

    update

---
+++ MyJava/src/shanksAlgorithmTest/ShanksTest.java
@@ -0,0 +1,14 @@
+package shanksAlgorithmTest;
+
+public class ShanksTest {
+
+	public static void main(String[] args) {
+		long G,n,alpha,beta;
+		shankes(G,n,alpha,beta);
+	}
+
+	private static void shankes(long g, long n, long alpha, long beta) {
+		
+	}
+
+}
