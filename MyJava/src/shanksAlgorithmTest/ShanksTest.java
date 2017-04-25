bcd3cd5410fe1a22ea43192922edd98fbd2c75e1
WheatleyZ <zhengsl7749@live.com>
2017-04-25 16:42:06 +0800

    update

--- MyJava/src/shanksAlgorithmTest/ShanksTest.java
+++ MyJava/src/shanksAlgorithmTest/ShanksTest.java
@@ -5,7 +5,8 @@ import java.math.*;
 public class ShanksTest {
 
 	public static void main(String[] args) {
-		System.out.println(shankes(809, 3, 525));
+		System.out.println("mod:" + 809 + " α:" + 3 + " β:" + 525);
+		System.out.println("result:" + shankes(809, 3, 525));
 	}
 
 	private static long shankes(long n, long alpha, long beta) throws ArrayIndexOutOfBoundsException {
